ea2f76c503878b343830ad6ca781bfbbbb33f31a
vijayshreesoni <vijayshreesoni.v@gmail.com>
2025-11-30 21:23:34 +0530

    Add gaushalas data structure with details

--- gaushaladata.js
+++ gaushaladata.js
@@ -1,1 +1,21 @@
 
+const gaushalas = [
+  {
+    name: "Shree Ram Gaushala",
+    distance: 2.5,
+    cows: 120,
+    calves: 30,
+    charges: "₹500 per cow",
+    contact: "9876543210",
+    address: "Near Nokha Road, Bikaner"
+  },
+  {
+    name: "Gokul Gaushala",
+    distance: 4.2,
+    cows: 80,
+    calves: 20,
+    charges: "₹400 per cow",
+    contact: "9876500000",
+    address: "Lalgarh, Bikaner"
+  }
+];
